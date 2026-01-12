0998410f794fb522f8979bb73a8c873e9df65b78
andre2ar <andre2ar@outlook.com>
2026-01-12 16:53:06 -0300

    Rename `verify-jwt.ts` to `VerifyJWT.ts` for consistency.

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -2,7 +2,7 @@ import {FastifyInstance} from "fastify";
 import {RegisterController} from "@/http/controllers/RegisterController";
 import {AuthenticateController} from "@/http/controllers/AuthenticateController";
 import {ProfileController} from "@/http/controllers/ProfileController";
-import {verifyJWT} from "@/http/middlewares/verify-jwt";
+import {verifyJWT} from "@/http/middlewares/VerifyJWT";
 
 export async function appRoutes(app: FastifyInstance) {
     app.post('/users', RegisterController);
