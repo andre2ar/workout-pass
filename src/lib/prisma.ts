fdece76598e9f022c178da70e19ff087a0a98394
andre2ar <andre2ar@outlook.com>
2026-01-13 15:18:52 -0300

    Refactor prisma adapter to use `config.DATABASE_URL` and update formatting.

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -3,10 +3,10 @@ import {config} from "@/config";
 import { PrismaPg } from '@prisma/adapter-pg'
 
 const adapter = new PrismaPg({
-    connectionString: process.env.DATABASE_URL
+    connectionString: config.DATABASE_URL
 });
 
 export const prisma = new PrismaClient({
     log: config.NODE_ENV === 'dev' ? ['query'] : [],
     adapter
-})
+});
