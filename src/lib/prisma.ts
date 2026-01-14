9af75f46ebd16966017fa0c2c6f42985b826e8f7
andre2ar <andre2ar@outlook.com>
2026-01-14 13:36:49 -0300

    Update Prisma adapter to support dynamic schema configuration via DATABASE_URL

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,9 +2,12 @@ import {PrismaClient} from '../../generated/prisma/client'
 import {config} from "@/config";
 import { PrismaPg } from '@prisma/adapter-pg'
 
-const adapter = new PrismaPg({
-    connectionString: config.DATABASE_URL
-});
+const schema = new URL(config.DATABASE_URL).searchParams.get('schema') || 'public';
+
+const adapter = new PrismaPg(
+    { connectionString: config.DATABASE_URL },
+    { schema },
+);
 
 export const prisma = new PrismaClient({
     log: config.NODE_ENV === 'dev' ? ['query'] : [],
